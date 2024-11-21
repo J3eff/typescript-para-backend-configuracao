954e4029bc86814ae250bdfbf6e55db904cacaab
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-21 00:26:01 -0300

    Criando uma entidade com decorators

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,8 +1,9 @@
 import { DataSource } from "typeorm";
+import PetEntity from "../entities/PetEntity";
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "./src/config/database.sqlite",
-    entities: [], // Mapeia as entidades no banco de dados.
+    entities: [PetEntity], // Mapeia as entidades no banco de dados.
     synchronize: true
 });
