14325cc0a298881408668cf9f5d3566ad5622622
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-21 10:38:19 -0300

    Finalizando o CRUD de Adotante

--- src/repositories/interfaces/InterfaceAdotanteRepository.ts
+++ src/repositories/interfaces/InterfaceAdotanteRepository.ts
@@ -1,5 +1,8 @@
 import AdotanteEntity from "../../entities/AdotanteEntity";
 
 export default interface InterfaceAdotanteRepository {
-    criarAdotante(adotante: AdotanteEntity): void | Promise<void>;
+    criaAdotante(adotante: AdotanteEntity): void | Promise<void>;
+    listaAdotantes(): AdotanteEntity[] | Promise<AdotanteEntity[]>;
+    atualizaAdotante(id: number, newData: AdotanteEntity): Promise<{ success: boolean, message?: string }> | void;
+    deletaAdotante(id: number): Promise<{ success: boolean, message?: string }> | void;
 }
