4a3ee4e677efb5122fb663d570960ba75485989e
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-21 10:02:15 -0300

    criando a entidade Adotante

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm";
 import PetEntity from "../entities/PetEntity";
+import AdotanteEntity from "../entities/AdotanteEntity";
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "./src/config/database.sqlite",
-    entities: [PetEntity], // Mapeia as entidades no banco de dados.
+    entities: [PetEntity, AdotanteEntity], // Mapeia as entidades no banco de dados.
     synchronize: true
 });
